bc2317b699e92ed1c5e7a8dbd161143480dbd1f5
Arek Karbowy <arek.karbowy@gmail.com>
2019-05-27 09:24:17 +0100

    init

---
+++ app/src/main/java/com/akarbowy/demosongs/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.akarbowy.demosongs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
